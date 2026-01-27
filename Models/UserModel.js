6d843df78081dd5f8b16fc035905a88ef50bfb3f
Rohit K Vinod <rohitkvinod2007@gmail.com>
2026-01-28 01:58:20 +0530

    fourth

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"
+import mongoose, { Schema } from "mongoose"
+import { boolean } from "zod"
 
 
 const userSchema=new mongoose.Schema({
@@ -16,13 +17,13 @@ const userSchema=new mongoose.Schema({
     },
     phoneNumber:{
         type:String,
-        required:true,
+        required:false,
         unique:true,
         trim:true,
     },
     password:{
         type:String,
-        required:true
+        required:false
     },
     role:{
         type:String,
@@ -33,6 +34,41 @@ const userSchema=new mongoose.Schema({
         type:Boolean,
         default:false,
     },
+    google_ID:{
+        type:String,
+        unique:true,
+    },
+    cart:[{
+        type: Schema.Types.ObjectId,
+        ref:"Cart",
+    }],
+    orderHistory:[{
+        type: Schema.Types.ObjectId,
+        ref:"Order",
+    }],
+    wishlist:[{
+        type:Schema.Types.ObjectId,
+        ref:"Wishlist"
+    }],
+    wallet:{
+        type:Number,
+        default:0,
+    },
+    createdOn:{
+        type:Date,
+        default:Date.now
+    },
+    referalCode:{
+        type:String,
+    },
+    redeemed_user:{
+        type: boolean,
+    },
+    redeemed_users:[{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }],
+
 },{timestamps:true})
 
 export default mongoose.model('User',userSchema)
