46a846da872c5b1a061a8d618cf8fb457ee0e76f
Rohit <rohit@Rohits-MacBook-Air.local>
2026-02-26 10:24:00 +0530

    updating home page

--- service/mailService.js
+++ service/mailService.js
@@ -1,31 +1,31 @@
-import nodemailer from "nodemailer"
-import dotenv, { decrypt } from "dotenv";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-export async function sendVerificationEmail(email,otp){
-    try {
-        const transporter= nodemailer.createTransport({
-            service:'gmail',
-            port:587,
-            secure:false,
-            requireTLS:true,
-            auth:{
-                user:process.env.NODEMAILER_EMAIL,
-                pass:process.env.NODEMAILER_PASSWORD
-            }
-        })
+export async function sendVerificationEmail(email, otp) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_PASSWORD
+      }
+    });
 
-        const info= await transporter.sendMail({
-            from:"Team RidePro",
-            to:email,
-            subject:"Verify your account",
-            text:`Your OTP is ${otp}`,
-            html:`<b>Your OTP : ${otp}</b>`
-        })
+    const info = await transporter.sendMail({
+      from: `"Team RidePro" <${process.env.NODEMAILER_EMAIL}>`,
+      to: email,
+      subject: "Verify your account",
+      text: `Your OTP is ${otp}`,
+      html: `<b>Your OTP: ${otp}</b>`
+    });
 
-        return info.accepted.length>0
-    } catch (error) {
-        console.log("Error in sending email : ",error)
-        return false
-    }
+    return info.accepted.length > 0;
+  } catch (error) {
+    console.log("Error in sending email:", error.message);
+    return false;
+  }
 }
